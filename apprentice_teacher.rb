4ab05b177550d8da432429bc6e0d04cab20cc846
Andrew Bruckman <abruckman09@gmail.com>
2016-11-23 16:43:49 -0800

    refactored with teacher class

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,26 +1,16 @@
 require_relative 'high_fiveable'
+require_relative 'teacher'
 
-class ApprenticeTeacher
+class ApprenticeTeacher < Teacher
   include HighFivable
 
-  attr_reader :age, :salary, :phase, :target_raise
-  attr_accessor :name
 
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
     @target_raise = 800
-    @phase = 3
+    @target_rating = 80
+    super
   end
 
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
 
   def teach_stuff
     response = ""
@@ -30,26 +20,6 @@ class ApprenticeTeacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
 
   def attend_training_session
     puts "Whoa. I know ruby-fu"
--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,26 +1,17 @@
+require_relative 'teacher'
 require_relative 'high_fiveable'
 
-class SeniorTeacher
+class SeniorTeacher < Teacher
   include HighFivable
 
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  attr_accessor :name
+  attr_reader  :performance_rating
 
   def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
     @target_raise = 1000
+    @target_rating = 90
+    super
   end
 
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
 
   def teach_stuff
     response = ""
@@ -30,26 +21,6 @@ class SeniorTeacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
 
   def lead_training_session
     puts "Hey newbie!  Here are some common pitfalls.  Don't fall in them!"
